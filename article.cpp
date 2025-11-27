17e80d9c8aeef46e04df2cbd2fb1cf9b7e93a098
Lodka234 <spitfire1@op.pl>
2025-11-27 07:01:44 +0000

    article

--- article.cpp
+++ article.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include "article.h"
-using namespace std;
-Article();
-    Article::Article(string articleTitle,Author articleAuthor,int year, string journalName)
-    Article::Article(Article &other);
-    void displayInfo();
-    string Article::getTitle();
-    Author Article::getAuthor();
-    int getPublicatinYear();
-    string Article::getJournal();
+ 
+Article ::Article(): title(""), author(Author("", "")), publicationYear(0), journal(""){}
+    Article ::Article(string articleTitle, Author articleAuthor, int year, string journalName): title(articleTitle), author(articleAuthor), publicationYear(year), journal(journalName){
+ 
+    }
+    Article ::Article(Article &other): title(other.title), author(other.author), publicationYear(other.publicationYear), journal(other.journal){}
+    string Article ::getTitle(){
+        return title;
+    }
+    Author Article :: getAuthor(){
+        return author;
+    }
+    int Article :: getPublicationYear(){
+        return publicationYear;
+    }
+    string Article ::getJournal(){
+        return journal;
+    }
